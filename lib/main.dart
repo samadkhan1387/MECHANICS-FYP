8e0e548ffce1570d6d32f5df91e165f3c9a9b9c9
ABDUL SAMAD <samad3316khan@gmail.com>
2023-10-21 01:37:39 +0500

    DESIGN CHANGES COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'AutoCareHub',
+      title: 'Mechanics Mangao',
       theme: theme(),
       // home: SplashScreen(),
       // We use routeName so that we don't need to remember the name
